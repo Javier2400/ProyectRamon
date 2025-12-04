934904bed8f309b8ff314522eb578b69a85ba629
Javier <st22030432@utlaguna.edu.mx>
2025-12-03 21:10:14 -0600

    ror

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -1,5 +1,15 @@
-import type { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 export const TypeOrmConfig = (): TypeOrmModuleOptions => ({
-    type: 'postgres',
+  type: 'postgres',
+
+  url: process.env.DATABASE_URL,
+
+  autoLoadEntities: true,
+
+  ssl: {
+    rejectUnauthorized: false,
+  },
+
+  synchronize: true, 
 });
